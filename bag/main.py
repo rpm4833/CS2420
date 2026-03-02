0c71f67e93dde61d83eb713ab1e641af9fc70c46
rpm4833 <doo483366@utahtech.edu>
2026-03-01 21:23:05 -0700

    fixed main in bag

--- bag/main.py
+++ bag/main.py
@@ -16,16 +16,6 @@ def main():
     end = time()
     print(f"Insertion took {(end - start):.2f} seconds. Total students: {bag.size()}")
 
-    # Deletion
-    start = time()
-    with open('DeleteNames.txt', 'r') as file:
-        for line in file:
-            temp = Student("", "", line.strip(), "", "")
-            if not bag.delete(temp):
-                print(f"Student with SSN {temp.ssn} not found!")
-    end = time()
-    print(f"Deletion took {(end - start):.2f} seconds. Total students: {bag.size()}")
-
     # Iteration
     start = time()
     ages = 0
@@ -35,6 +25,16 @@ def main():
         total += 1
     end = time()
     print(f"Iteration took {(end - start):.4f} seconds. Average age of all students: {(ages / total):.4}")
+
+    # Deletion
+    start = time()
+    with open('DeleteNames.txt', 'r') as file:
+        for line in file:
+            temp = Student("", "", line.strip(), "", "")
+            if not bag.delete(temp):
+                print(f"Student with SSN {temp.ssn} not found!")
+    end = time()
+    print(f"Deletion took {(end - start):.2f} seconds. Total students: {bag.size()}")
     
     # Retrieval
     start = time()
